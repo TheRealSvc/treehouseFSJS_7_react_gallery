384b15b5a809609d7279a1cbb3023939d746bdfe
TheRealSvc <svcoelln@gmail.com>
2021-05-01 18:50:52 +0200

    initial commit

---
+++ react_gallery_app/src/components/Photo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import TeacherList from '../data/teachers';
+
+const Photo = () => {
+    return (  
+      <li className="photo-container" >
+        <img src={"https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"} alt="photograph" />
+      </li>
+    );
+}
+
+export default Teachers;
